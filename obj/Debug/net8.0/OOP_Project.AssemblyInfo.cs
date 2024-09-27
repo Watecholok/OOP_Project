33a2feb84b7bd9b9233d0d2fc44275122ee222ef
Watecholok <ethanlee.zj@gmail.com>
2024-09-27 10:48:14 +0800

    uh

--- obj/Debug/net8.0/OOP_Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/OOP_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba8b9f26c6efce664370a927fff801c7b5838cf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
